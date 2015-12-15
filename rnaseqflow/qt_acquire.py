20722fd73771dbceec7bd552fb136d57782f0ee4
jpalpant <jbp25@duke.edu>
2015-12-15 02:22:30 -0500

    Implement package acquisition with Choco for Windows

--- rnaseqflow/qt_acquire.py
+++ rnaseqflow/qt_acquire.py
@@ -26,8 +26,8 @@ def get_pyqt():
 
 def get_with_brew(program):
     try: 
-        print 'Beginning installation of {0}'.format(program)
-        pid = subprocess.check_output(['brew', 'install', program])
+        print 'Beginning installation of {0} with brew'.format(program)
+        subprocess.check_output(['brew', 'install', program])
     except CalledProcessError as e:
         print 'CalledProcessError error({0}) during command'.format(e.returncode, e.cmd)
         print 'Error output: {0}'.format(e.output)
@@ -35,6 +35,21 @@ def get_with_brew(program):
         print "OSError error({0}): {1}".format(e.errno, e.strerror)
         print 'Likely brew is not installed'
         print 'Please install Homebrew - http://brew.sh/' 
+    else:
+        print 'Installation of {0} successful!'.format(program)
+        
+        
+def get_with_choco(program):
+    try:
+        print 'Beginning installation of {0} with choco'.format(program)
+        subprocess.check_output(['choco', 'install', program])
+    except CalledProcessError as e:
+        print 'CalledProcessError error({0}) during command'.format(e.returncode, e.cmd)
+        print 'Error output: {0}'.format(e.output)
+    except OSError as e:
+        print "OSError error({0}): {1}".format(e.errno, e.strerror)
+        print 'Likely Chocolatey is not installed'
+        print 'Please install Chocolatey - https://chocolatey.org/' 
     else:
         print 'Installation of {0} successful!'.format(program)
 
